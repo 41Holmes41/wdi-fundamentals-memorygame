a6ab4cf1b979a2e2b422c59b981af465634ded60
Cole Holmes <mitchellcholmes@gmail.com>
2019-06-09 15:12:18 -0500

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,19 +4,7 @@ var cards=["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne)
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo)
-
-if(cardsInPlay.length===2) {
+function checkForMatch() {
 
 	if (cardsInPlay[0]===cardsInPlay[1]) {
 		alert("You found a match!");
@@ -26,3 +14,21 @@ if(cardsInPlay.length===2) {
 		alert("Sorry Try again!");
 	}
 }
+
+
+function flipCard(cardId) {
+
+	console.log("User flipped "+ cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length===2) {
+
+		checkForMatch();
+	
+	};
+
+};
+
+flipCard(0);
+flipCard(2);
